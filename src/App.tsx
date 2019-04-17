bce98f7b1f524e3a0c5e9294b43d83c92c887664
David Leger <dleger@manifold.co>
2019-04-17 17:43:51 -0300

    initial page setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Discover from "./pages/Discover";
+import ManageResource from "./pages/ManageResource";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <manifold-connection env="stage">
+            <Route path="/" component={Discover} />
+            <Route
+              path="/resources/:resourceId/edit"
+              component={ManageResource}
+            />
+          </manifold-connection>
+        </Router>
       </div>
     );
   }
---
+++ src/pages/Discover.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Discover = () => {
+  return <manifold-marketplace />;
+};
+
+export default Discover;
